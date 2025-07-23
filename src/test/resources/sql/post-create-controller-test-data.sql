e8b1d43e143b7559bdd0ab90ed577d72524fc301
JIHO CHOI <wlghsp@gmail.com>
2025-07-23 20:55:56 +0900

    PostController, PostCreateController 테스트 코드 추가

---
+++ src/test/resources/sql/post-create-controller-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'wlghsp@knou.ac.kr', 'aprilgone', 'Daejeon', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
