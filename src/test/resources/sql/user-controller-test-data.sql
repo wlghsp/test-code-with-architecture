36db02ea60702dbcea636d980eda932fdf528ca7
JIHO CHOI <jh.choi2@okestro.com>
2025-07-23 19:54:48 +0900

    userCo가troller 테스트 코드 추

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'wlghsp@knou.ac.kr', 'aprilgone', 'Daejeon', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'wlghsp@naver.com', 'aprilend', 'Daejeon', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
