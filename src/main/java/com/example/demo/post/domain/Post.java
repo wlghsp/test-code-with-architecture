5222f08b903935f25e816dd74e9f3b0841561800
JIHO CHOI <jh.choi2@okestro.com>
2025-07-28 13:32:24 +0900

    Fake를 이용한 PostService 테스트 작성

--- src/main/java/com/example/demo/post/domain/Post.java
+++ src/main/java/com/example/demo/post/domain/Post.java
@@ -1,10 +1,9 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.common.service.port.ClockHolder;
 import com.example.demo.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.Clock;
 
 @Getter
 public class Post {
@@ -23,21 +22,21 @@ public class Post {
         this.writer = writer;
     }
 
-    public static Post from(User writer, PostCreate postCreate) {
+    public static Post from(User writer, PostCreate postCreate, ClockHolder clockHolder) {
         return Post.builder()
                 .content(postCreate.getContent())
                 .writer(writer)
-                .createdAt(Clock.systemUTC().millis())
+                .createdAt(clockHolder.millis())
                 .build();
     }
 
-    public Post update(PostUpdate postUpdate) {
+    public Post update(PostUpdate postUpdate, ClockHolder clockHolder) {
         return Post.builder()
                 .id(id)
                 .content(postUpdate.getContent())
                 .writer(writer)
                 .createdAt(createdAt)
-                .modifiedAt(Clock.systemUTC().millis())
+                .modifiedAt(clockHolder.millis())
                 .writer(writer)
                 .build();
     }
--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,10 +1,9 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,14 +27,47 @@ class PostTest {
                 .build();
 
         // when
-        Post post = Post.from(writer, postCreate);
+        Post post = Post.from(writer, postCreate, new TestClockHolder(1678530673958L));
 
         // then
         assertThat(post.getContent()).isEqualTo("helloworld");
+        assertThat(post.getCreatedAt()).isEqualTo(1678530673958L);
         assertThat(post.getWriter().getEmail()).isEqualTo("wlghsp@knou.ac.kr");
         assertThat(post.getWriter().getNickname()).isEqualTo("aprilgone");
         assertThat(post.getWriter().getAddress()).isEqualTo("Daejeon");
         assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
         assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+    }
+
+    @Test
+    void PostUpdate으로_게시물을_만들_수_있다() {
+        // given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("hello word :)")
+                .build();
+
+        User writer = User.builder()
+                .id(1L)
+                .email("wlghsp@knou.ac.kr")
+                .nickname("aprilgone")
+                .address("Daejeon")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .content("helloworld")
+                .createdAt(1678530673958L)
+                .modifiedAt(0L)
+                .writer(writer)
+                .build();
+
+        // when
+        post = post.update(postUpdate, new TestClockHolder(1678530673958L));
+
+        // then
+        assertThat(post.getContent()).isEqualTo("hello word :)");
+        assertThat(post.getModifiedAt()).isEqualTo(1678530673958L);
     }
 }
